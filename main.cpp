b2e087134b5ce96161808ff1e481610138a5c0a6
ivan.andreyshev <ivan.andreyshev@ispring.com>
2023-05-21 21:30:49 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "complex_number.h"
+
+int main(int argc, char const *argv[])
+{
+    ComplexNumber number = ComplexNumber(10, 15);
+    number.Display();
+    return 0;
+}
